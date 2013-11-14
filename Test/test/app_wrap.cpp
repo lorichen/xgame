445f8ae2987d6b2b715ccf6747cb8e15f1b05b0a
lorichen <kevin_chen@foxmail.com>
2013-11-14 11:27:06 +0800

    sync update from compony computer

--- Test/test/app_wrap.cpp
+++ Test/test/app_wrap.cpp
@@ -100,8 +100,9 @@ bool AppWrap::init(void* hwnd)
 	xs::Point pt;
 	xs::Rect  rc(0,0,gs_width,gs_height);
 
-	//gs_pTex = g_psRenderSystem->getTextureManager()->createTextureFromFile("13.png");
+	gs_pTex = g_psRenderSystem->getTextureManager()->createTextureFromFile("13.png");
 
+	/*
 	bool bLoadMap = g_psScenemanager->loadScene(mpFile.c_str(),wpFile.c_str(),&rc,NULL,false,&pt);
 	if(!bLoadMap)
 	{
@@ -109,6 +110,7 @@ bool AppWrap::init(void* hwnd)
 		printf("\n ¼ÓÔØ³¡¾°Ê§°Ü£¡");
 		return false;
 	}
+	*/
 	
 
 	return true;
@@ -124,11 +126,11 @@ void AppWrap::update(int tick,int delta_ms)
 	g_psRenderSystem->setSceneBlending(SBF_SOURCE_ALPHA,SBF_ONE_MINUS_SOURCE_ALPHA);
 
 	//g_psRenderSystem->point(xs::Point(100,100),xs::ColorValue(1.0,0,0,1.0));
-	//g_psRenderSystem->line(xs::Point(0,0),xs::Point(800,600),ColorValue(1,0,0,1));
+	g_psRenderSystem->line(xs::Point(0,0),xs::Point(800,600),ColorValue(1,0,0,1));
     
-    //xs::Rect rc(0,0,100,100);
+    xs::Rect rc(0,0,100,100);
 	//g_psRenderSystem->box(rc,ColorValue(1,0,0,1));
-	//g_psRenderSystem->rectangle(rc,gs_pTex);
+	g_psRenderSystem->rectangle(rc,gs_pTex);
 
 	//---------------------------------------------
 
